e528fb0f1ec3873b259027f9fc895346c1ffbe3c
Patutechz <patrickokpunor65@gmail.com>
2024-10-03 00:59:02 +0100

    Modified server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const Flutterwave = require("flutterwave-node-v3");
+// const Flutterwave = require("flutterwave-node-v3");
 const cors = require("cors");
 const billRoutes = require('./routes/bills');
 
@@ -9,7 +9,7 @@ const billRoutes = require('./routes/bills');
 const app = express();
 
 // flutterwave
-const flw = new Flutterwave(process.env.PUBLIC_KEY, process.env.SECRET_KEY);
+// const flw = new Flutterwave(process.env.PUBLIC_KEY, process.env.SECRET_KEY);
 
 //middleware
 app.use(cors());
