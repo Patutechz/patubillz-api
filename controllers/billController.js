5bf7ec95fcc8a4ae792bb51de79d280404878fee
Patutechz <patrickokpunor65@gmail.com>
2024-10-03 20:02:43 +0100

    modified bill routes and billController

--- controllers/billController.js
+++ controllers/billController.js
@@ -15,6 +15,66 @@ const getBillsCategories = async (req, res) => {
   }
 };
 
+// validate bill
+const validateBill = async (req, res) => {
+  try {
+    const payload = {
+      "item_code": "MD154",
+      "code": "BIL111",
+      "customer": "08189810712"
+  };
+
+    const response = await flw.Bills.validate(payload);
+    res.json(response);
+    // res.status(200).json(response);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+    // res.status(400).json({error : error.message});
+  }
+};
+
+// create bill
+const createBill = async (req, res) => {
+  try {
+    const payload = {
+      country: "NG",
+      customer: "2348189810712",
+      amount: 50,
+      recurrence: "ONCE",
+      type: "AIRTIME",
+      reference: "9300ko984",
+    };
+
+    const response = await flw.Bills.create_bill(payload);
+    // res.json(response);
+    res.status(200).json(response);
+    console.log(response);
+  } catch (error) {
+    // console.log(error);
+    res.status(400).json({error : error.message});
+  }
+};
+
+// get status
+const getStatus = async (req, res) => {
+  try {
+    const payload = {
+      reference: "9300049404444",
+    };
+
+    const response = await flw.Bills.fetch_status(payload);
+    res.json(response);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = {
   getBillsCategories,
+  validateBill,
+  createBill,
+  getStatus,
 };
--- routes/bills.js
+++ routes/bills.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const {
   getBillsCategories,
+  validateBill,
+  createBill,
+  getStatus,
 } = require("../controllers/billController");
 
 const router = express.Router();
 
 // Get All bills
 router.get("/", getBillsCategories);
+
+// Validate bill
+router.get("/validate", validateBill);
+
+// Post a single workout
+router.post('/paybill' , createBill);
+
+// Get status
+router.get("/getstatus", getStatus);
 
 module.exports = router;
